97f6017af2652d07f0fc70939728f537f91cdace
emremrdl <alsdnr2920@gmail.com>
2024-04-20 16:57:39 +0900

    MeloTTS 추가

---
+++ MeloTTS/melo/infer.py
@@ -0,0 +1,17 @@
+import os
+import click
+from MeloTTS.melo.api import TTS
+
+    
+def inference(ckpt_path, text, language, output_dir):
+    if ckpt_path is None:
+        raise ValueError("The model_path must be specified")
+    
+    config_path = os.path.join(os.path.dirname(ckpt_path), 'config.json')
+    model = TTS(language=language, config_path=config_path, ckpt_path=ckpt_path)
+    
+    for spk_name, spk_id in model.hps.data.spk2id.items():
+        save_path = f'{output_dir}/output.wav'
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        model.tts_to_file(text, spk_id, save_path)
+
---
+++ MeloTTS/melo/train.sh
@@ -0,0 +1,20 @@
+CONFIG=$1
+GPUS=$2
+MODEL_NAME=$(basename "$(dirname $CONFIG)")
+
+PORT=10902
+
+while : # auto-resume: the code sometimes crash due to bug of gloo on some gpus
+do
+torchrun --nproc_per_node=$GPUS \
+        --master_port=$PORT \
+    train.py --c $CONFIG --model $MODEL_NAME --pretrain_G ./logs/pretrain/G.pth \
+	--pretrain_D ./logs/pretrain/D.pth --pretrain_dur ./logs/pretrain/DUR.pth
+
+for PID in $(ps -aux | grep $CONFIG | grep python | awk '{print $2}')
+do
+    echo $PID
+    kill -9 $PID
+done
+sleep 30
+done
